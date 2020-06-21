492295cce80f9ec3d3d19bbeb6cd65b2d73133c2
Victoria Rybalko <rybalko.victoria@gmail.com>
2020-06-21 19:47:30 +0300

    Add switch-case construction and final set of tasks
    Added JSDocs, js code, console logs of the test cases results.

--- js/switch-case-construction.js
+++ js/switch-case-construction.js
@@ -16,3 +16,126 @@
  8. Дана строка из 6-ти цифр. Проверьте, что сумма первых трех цифр равняется сумме вторых трех цифр. Если это так - выведите 'да', в противном случае выведите 'нет'.
 
  */
+
+//Task 1
+/**
+ * Variable 'result' is used to store the string with the name of the season in Russian. Variable 'num' is used to store the number of a season in a year.
+ */
+let result;
+const num = '2';
+/**
+ * Assigns the value to result variable (the name of the season of the year in Russian) based on the value of num variable (number of a season in a year).
+ */
+switch (num) {
+    case '1':
+        result = 'зима';
+        break;
+    case '2':
+        result = 'весна';
+        break;
+    case '3':
+        result = 'лето';
+        break;
+    case '4':
+        result = 'осень';
+        break;
+    default:
+        console.log("invalid number");
+}
+
+console.log(result);
+
+//Task 3
+
+
+/**
+ * @type {number} Ordinal number of day in a month
+ */
+const day = 25;
+/**
+ * Logs into console the message about the ordinal number of a decade based on the number of day in a month
+ */
+if (day > 0 && day < 11) {
+    console.log('Первая декада');
+} else if (day > 10 && day < 21) {
+    console.log('Вторая декада');
+} else if (day > 20 && day < 32) {
+    console.log('Третья декада');
+} else {
+    console.log('Invalid day number');
+}
+
+//Task 4
+
+/**
+ * @type {number} Ordinal number of month in a year
+ */
+const month = 12;
+/**
+ * The following expression logs into console the season of the year in Russian based on the given ordinal number of a month.
+ */
+if (month === 12 || (month > 0 && month <= 2)) {
+    console.log('зима');
+} else if (month > 2 && month <= 5) {
+    console.log('весна');
+} else if (month > 5 && month <= 8) {
+    console.log('лето');
+} else if (month > 8 && month <= 11) {
+    console.log('осень');
+}
+
+//Task 5
+
+const str1 = 'abcde';
+
+/**
+ * The following expression checks whether the first character of the string is 'a'. Logs into console 'Да' if true, and 'Нет' if false.
+ */
+if (str1[0] === 'a') {
+    console.log('Да');
+} else {
+    console.log('Нет');
+}
+
+//Task 6
+
+const str2 = '123456';
+
+/**
+ * The followong expression checks whether the first character of a string is either '1', or '2', or '3'. Logs into console 'Да' if true, and 'Нет' if false.
+ */
+if (str2[0] === '1' || str2[0] === '2' || str2[0] === '3') {
+    console.log('Да');
+} else {
+    console.log('Нет');
+}
+
+//Task 7
+
+/**
+ * Sums first three digits in a string
+ * @param string
+ * @returns {number} Arithmetic sum of first three characters of a given string.
+ */
+function stringToNumberSum(string) {
+    return Number(string[0]) + Number(string[1]) + Number(string[2]);
+}
+
+console.log('stringToNumberSum(\'587\') = ' + stringToNumberSum('587'));
+console.log('stringToNumberSum(\'995\') = ' + stringToNumberSum('995'));
+
+//Task 8
+
+str3 = '286943';
+
+/**
+ * The following expression checks whether the sum of the first three digits in a string is equal to the sum of the second three digits in a string. Logs into console 'да' if true, and 'нет' if false.
+ */
+if ((Number(str3[0]) + Number(str3[1]) + Number(str3[2])) === (Number(str3[3]) + Number(str3[4]) + Number(str3[5]))) {
+    console.log('да');
+} else {
+    console.log('нет');
+}
+
+
+
